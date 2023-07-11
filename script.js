691ec49863802e539dfcb4406420b6622e28d0f8
shafi <shaifk108@gmail.com>
2023-07-11 12:42:02 +0600

    extra checking calculator

--- script.js
+++ script.js
@@ -1,3 +1,113 @@
+// //Calcolater
+// const currentNum = document.querySelector('.currentNum');
+// const result = document.querySelector('.result');
+// // ==============
+// const equalize = document.querySelector('.equalize');
+// ======================
+// const num1 = document.querySelector('.num1');
+// const num2 = document.querySelector('.num2');
+// const num3 = document.querySelector('.num3');
+// const num4 = document.querySelector('.num4');
+// const num5 = document.querySelector('.num5');
+// const num6 = document.querySelector('.num6');
+// const num7 = document.querySelector('.num7');
+// const num8 = document.querySelector('.num8');
+// const num9 = document.querySelector('.num9');
+// const num0 = document.querySelector('.num0');
+// const plus = document.querySelector('.plus');
+// const minas = document.querySelector('.minas');
+// const gon = document.querySelector('.gon');
+// const vag = document.querySelector('.vag');
+// const ac = document.querySelector('.ac');
+
+// const num11 = num1.addEventListener('click', ()=>{
+//   return '1';
+// })
+// console.log(num11())
+// num2.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='2';
+// })
+// num3.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='3';
+// })
+// num4.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='4';
+// })
+// num5.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='5';
+// })
+// num6.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='6';
+// })
+// num7.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='7';
+// })
+// num8.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='8';
+// })
+// num9.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='9';
+// })
+// num0.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='0';
+// })
+// plus.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='+';
+// })
+// minas.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='-';
+// })
+// gon.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='*';
+// })
+// vag.addEventListener('click', ()=>{
+//   currentNum.innerHTML ='/';
+// })
+// ac.addEventListener('click', ()=>{
+//   currentNum.classList.remove('numberOto')
+//   result.classList.remove('finalResult')
+// })
+
+
+
+// equalize.addEventListener('click', ()=>{
+
+//   currentNum.classList.add('numberOto')
+//   result.classList.add('finalResult')
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Text Fixer
 
 const symbolRm = document.querySelector('.symbolRm');
 const symbolBtn = document.querySelector('.symbolBtn');
